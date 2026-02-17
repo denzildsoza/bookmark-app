dd6d27e00d3d665021996441f6783be233ae2c23
denzildsouza <115543290+denzildsoza@users.noreply.github.com>
2026-02-17 20:18:26 +0530

    vercel env

--- src/utills/getBaseUrl.js
+++ src/utills/getBaseUrl.js
@@ -2,9 +2,10 @@ const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_SITE_URL)
     return process.env.NEXT_PUBLIC_SITE_URL.replace(/\/$/, "");
 
-  if (process.env.NEXT_PUBLIC_VERCEL_URL)
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  if (process.env.VERCEL_URL)
+    return `https://${process.env.VERCEL_URL}`;
 
   return "http://localhost:3000";
 };
+
 export default getBaseUrl;
